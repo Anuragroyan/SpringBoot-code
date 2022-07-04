ddbb8f57fe025422827673fa1285bf330094fc20
Anuragroy1 <anuragroy.xworkx@gmail.com>
2022-07-04 21:44:08 +0530

    course database complete

--- api-calls-database/apicall-database-springboot/src/main/java/com/example/apicalldatabasespringboot/Coure_database/Course/courseRepository.java
+++ api-calls-database/apicall-database-springboot/src/main/java/com/example/apicalldatabasespringboot/Coure_database/Course/courseRepository.java
@@ -1,7 +1,15 @@
 package com.example.apicalldatabasespringboot.Coure_database.Course;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
-public interface courseRepository extends CrudRepository {
+public interface courseRepository extends CrudRepository<Course, String> {
+
+    public List<Course> findByTopicId(String topicId);
+
+    public Course findOne(String id);
+
+    public void delete(String id);
     
 }
--- api-calls-database/apicall-database-springboot/src/main/java/com/example/apicalldatabasespringboot/Coure_database/Course/courseService.java
+++ api-calls-database/apicall-database-springboot/src/main/java/com/example/apicalldatabasespringboot/Coure_database/Course/courseService.java
@@ -1,5 +1,38 @@
 package com.example.apicalldatabasespringboot.Coure_database.Course;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class courseService {
+
+    @Autowired
+    private courseRepository courserepository;
+
+    public List<Course> getAllCourse(String topicId) {
+        List<Course> course = new ArrayList<>();
+        courserepository.findByTopicId(topicId)
+        .forEach(course::add);
+        return course;
+    }
+
+    public Course getCourse(String id) {
+        return courserepository.findOne(id);
+    }
+
+    public void addCourse(Course course) {
+        courserepository.save(course);
+    }
+
+    public void updateCourse(Course course) {
+        courserepository.save(course);
+    }
+
+    public void deleteCourse(String id) {
+        courserepository.delete(id);
+    }
     
 }
